58c07a7c08b536b197973f4c9b43846f5270fa86
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-06-01 18:26:09 +0530

    Day2

---
+++ Typescript/Day2/demo1.ts
@@ -0,0 +1,45 @@
+
+// console.log('Welcome to My Typescript Session...');
+/*
+var a;
+var a1;
+var a$;
+var a_;
+var a;*/
+ 
+//Data Types 
+// number(int,long,double,float,short),string (string,char=>""/'')
+//,boolean(true,false),any(number,string,boolean),void(It does not point any thing)
+// misec. data types=> null & undefined
+
+var b:number=10; //Forward Declaration & defination
+
+// console.log("Value of B is "+b);
+
+// console.log(`
+// ------------------------
+// Value of B is ${b} =======
+// `);
+
+var b1=10;
+var b3='Sumit';
+var b4:string;
+var b5:number;
+
+// console.log('Value of b5 is '+b5);
+var b6;
+b6=2.5;
+console.log('Value of b6 is '+b6);
+b6='Sumit';
+console.log('Value of b6 is '+b6);
+
+
+
+
+
+
+
+
+
+
+
