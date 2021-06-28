5b6af1aea1fca31b6a4ed8e2078f85dbc7c7a433
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-06-28 18:42:11 +0530

    Day8

---
+++ Angular/Day8/fordirective/fordirective.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fordirective',
+  templateUrl: './fordirective.component.html',
+  styleUrls: ['./fordirective.component.css']
+})
+export class FordirectiveComponent implements OnInit {
+
+  course=["Angular 8","React Js","Embber Js","Vue Js","JSP"];
+
+  arrobj=[
+    {
+      name:"Samsung",
+      price:20000,
+      quantity:1
+    },
+    {
+      name:"Motorola",
+      price:22000,
+      quantity:1
+    },
+    {
+      name:"OnePlus",
+      price:25000,
+      quantity:1
+    },
+    {
+      name:"RealeMe",
+      price:27000,
+      quantity:1
+    },
+    {
+      name:"Iphone",
+      price:67000,
+      quantity:1
+    },
+    
+  ];
+  selected:string='';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onMouseOver(item){
+    console.log("Mouse Over Event Occur...");
+    this.selected=item.name;
+  }
+  onMouseOut(){
+    this.selected='';
+  }
+
+}
